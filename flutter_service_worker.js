6ac782e8f70b432b9db7c8087469d031e7c9dab0
Davood K <45532116+dvidfrmr@users.noreply.github.com>
2022-05-26 22:38:53 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "5db5e8f3104ffa8daf22ffc6f0765e39",
-"index.html": "c104bc16338d244e691a1181c544eb28",
-"/": "c104bc16338d244e691a1181c544eb28",
-"main.dart.js": "9415b303c2eeed396d0a36ca3f2e0cb5",
+"index.html": "9505a3ef777e2f8bae1265f9919ca8f4",
+"/": "9505a3ef777e2f8bae1265f9919ca8f4",
+"main.dart.js": "f2428db703876dd2bab076796b4b0a1e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
